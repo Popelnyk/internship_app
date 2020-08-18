9cca32970ef602ef9577180992fb6b1bea88428e
Alexander <alexy-lex2013@yandex.ru>
2020-08-18 17:36:41 +0300

    Site logo + google logo

--- app/src/app/components/pages/auth-page/auth-page.component.ts
+++ app/src/app/components/pages/auth-page/auth-page.component.ts
@@ -7,7 +7,7 @@ import {AuthService} from "../../../services/auth.service";
   styleUrls: ["./auth-page.component.scss"],
 })
 export class AuthPageComponent implements OnInit{
-  backgroundImageUrl: string = "https://images.wallpaperscraft.ru/image/gory_zakat_nebo_gorizont_118121_1920x1080.jpg";
+  backgroundImageUrl: string = "https://images.wallpaperscraft.ru/image/les_tropinka_derevya_118806_1920x1080.jpg";
 
   constructor(public authService: AuthService) { }
   ngOnInit() { }
