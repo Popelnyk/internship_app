b81926330178c528f19ed9cdb96a5d48db92ac23
Alexander <alexy-lex2013@yandex.ru>
2020-08-24 18:42:22 +0300

    cordova service for detecting cordova as global object and working with mobile app

--- app/src/app/components/pages/auth-page/auth-page.component.ts
+++ app/src/app/components/pages/auth-page/auth-page.component.ts
@@ -1,5 +1,6 @@
 import {Component, getPlatform, OnInit} from "@angular/core";
 import {AuthService} from "../../../services/auth.service";
+import {CordovaService} from "../../../services/cordova.service";
 
 @Component({
   selector: "app-auth-page",
@@ -9,11 +10,13 @@ import {AuthService} from "../../../services/auth.service";
 export class AuthPageComponent implements OnInit{
   backgroundImageUrl: string = "https://images.wallpaperscraft.ru/image/les_tropinka_derevya_118806_1920x1080.jpg";
   googleLogoUrl: string = "https://img.icons8.com/clouds/200/000000/google-logo.png";
+  cordovaPlugins: string = '';
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService, public cordovaService: CordovaService) { }
 
   ngOnInit() {
     if ((<any>window)._cordovaNative) {
+      this.cordovaPlugins = Object.keys(<any>window).join(', ');
       this.authService.updateUser();
     }
   }
