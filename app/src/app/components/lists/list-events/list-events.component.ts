7e4c969b83432ff2e77f653069e8315df1e66a31
Alexander <alexy-lex2013@yandex.ru>
2020-08-26 03:51:07 +0300

    Updated task board Added styles for Event item

--- app/src/app/components/lists/list-events/list-events.component.ts
+++ app/src/app/components/lists/list-events/list-events.component.ts
@@ -6,5 +6,6 @@ import {Component, OnInit} from "@angular/core";
   styleUrls: ["./list-events.component.scss"],
 })
 export class ListEventsComponent {
-
+  testEventsList: Array<any> = [{name: "foo1 bar tar loe", description: "bar is bar bar is bar bar is bar", date: "12.01.2002"},
+    {name: "foo2", description: "bar is bar2", date: "11.11.2442"}];
 }
